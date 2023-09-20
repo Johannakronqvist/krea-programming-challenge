339e4eb7788d9782e639c20eacd7b9e188ba7943
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2023-09-20 16:05:05 +0200

    added checkForBingo and findRow function

--- index.mjs
+++ index.mjs
@@ -22,14 +22,63 @@ for (let i = 0; i < boardData.length; i += boardLength) {
 
   while (board.length < boardLength) {
     //console.log('currentIndex', currentIndex);
-    let currentNum = parseInt(boardData[currentIndex]);
+    let currentNum = boardData[currentIndex];
     board.push({ [currentNum]: false });
     currentIndex += 1;
   }
 
   boards.push(board);
-  console.log(boards);
 }
+
+//GAME
+//Draw numbers
+const drawNumbers = [...testDrawNumber];
+let currentDrawNumber = null;
+
+const findRow = (idx) => {
+  let findChecked = 0;
+  const board = boards[idx];
+  let i = 0;
+
+  while (findChecked < 5) {
+    if (Object.keys(board[i])) {
+      findChecked += 1;
+    } else {
+      findChecked = 0;
+    }
+
+    if (i === board.length) break;
+  }
+
+  if (findChecked === 5) return true;
+};
+
+//Check for bingo
+let bingo = false;
+const checkForBingo = (idx) => {
+  if (findRow(idx)) {
+    boards.splice(idx, 1);
+    bingo = true;
+  }
+};
+
+drawNumbers.forEach((num) => {
+  currentDrawNumber = num;
+
+  while (!bingo) {
+    //Set all numbers to true if they are they are drawn
+    boards.forEach((board, idx) => {
+      board.forEach((obj) => {
+        const boardNum = parseInt(Object.keys(obj));
+
+        if (boardNum === currentDrawNumber) {
+          obj[boardNum] = true;
+          checkForBingo(idx);
+        }
+      });
+    });
+  }
+});
 
 //INPUT Draw numbers: Array
 //INPUT Boards: string of numbers with rows and empty rows between boards
@@ -43,6 +92,7 @@ for (let i = 0; i < boardData.length; i += boardLength) {
 
 //GAME STEPS to reach last board:
 /**
+ * pre-step(if a row or column is full (and there is more than 1 board left) remove from game)
  * 1. draw a number
  * 2. fill in the number on each board that has it
  * repeat until a full row or column is found. If more than one board is still in the game, remove that board from the game.
