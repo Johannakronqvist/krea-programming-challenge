a9f9ea3fc70e565e01a9db04c2fc3d03a6290091
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2023-09-21 09:00:21 +0200

    created calculateBoardScore func

--- index.mjs
+++ index.mjs
@@ -12,7 +12,9 @@ boardData = boardData
   .split(' ');
 
 const boardLength = 25;
-const boards = [];
+let boards = [];
+let boardsToRemove = [];
+let bingo = false;
 
 //Create inital boards
 for (let i = 0; i < boardData.length; i += boardLength) {
@@ -21,7 +23,6 @@ for (let i = 0; i < boardData.length; i += boardLength) {
   let currentIndex = i;
 
   while (board.length < boardLength) {
-    //console.log('currentIndex', currentIndex);
     let currentNum = boardData[currentIndex];
     board.push({ [currentNum]: false });
     currentIndex += 1;
@@ -35,50 +36,112 @@ for (let i = 0; i < boardData.length; i += boardLength) {
 const drawNumbers = [...testDrawNumber];
 let currentDrawNumber = null;
 
-const findRow = (idx) => {
-  let findChecked = 0;
-  const board = boards[idx];
-  let i = 0;
+const findRow = () => {
+  boards.forEach((board, idx) => {
+    let findChecked = 0;
 
-  while (findChecked < 5) {
-    if (Object.keys(board[i])) {
-      findChecked += 1;
-    } else {
+    for (let i = 0; i < board.length; i += 5) {
       findChecked = 0;
-    }
 
-    if (i === board.length) break;
-  }
-
-  if (findChecked === 5) return true;
-};
-
-//Check for bingo
-let bingo = false;
-const checkForBingo = (idx) => {
-  if (findRow(idx)) {
-    boards.splice(idx, 1);
-    bingo = true;
-  }
-};
-
-drawNumbers.forEach((num) => {
-  currentDrawNumber = num;
-
-  while (!bingo) {
-    //Set all numbers to true if they are they are drawn
-    boards.forEach((board, idx) => {
-      board.forEach((obj) => {
-        const boardNum = parseInt(Object.keys(obj));
-
-        if (boardNum === currentDrawNumber) {
-          obj[boardNum] = true;
-          checkForBingo(idx);
+      for (let j = 0; j < 5; j++) {
+        if (Object.values(board[i + j])[0]) {
+          findChecked += 1;
+          if (findChecked === 5) {
+            boardsToRemove.push(idx);
+            bingo = true;
+          }
+        } else {
+          findChecked = 0;
         }
-      });
+      }
+    }
+  });
+};
+
+const findColumn = () => {
+  boards.forEach((board, idx) => {
+    let findChecked = 0;
+
+    for (let i = 0; i < 5; i++) {
+      findChecked = 0;
+
+      for (let j = 0; j < 25; j += 5) {
+        if (Object.values(board[i + j])[0]) {
+          findChecked += 1;
+          if (findChecked === 5) {
+            boardsToRemove.push(idx);
+            bingo = true;
+          }
+        } else {
+          findChecked = 0;
+        }
+      }
+    }
+  });
+};
+
+const markNumbers = (currentDrawNumber) => {
+  console.log('in markNumbers');
+  //Set all numbers to true if they are they are drawn
+  boards.forEach((board) => {
+    board.forEach((obj) => {
+      const boardNum = parseInt(Object.keys(obj)[0]);
+
+      if (boardNum === currentDrawNumber) {
+        obj[boardNum] = true;
+      }
     });
+  });
+};
+
+const checkForBingo = () => {
+  console.log('in checkForBingo');
+
+  findRow();
+  findColumn();
+};
+
+const removeBingoBoards = () => {
+  console.log('in removeBingoBoard');
+  boards = boards.filter((board, idx) => !boardsToRemove.includes(idx));
+  boardsToRemove = [];
+};
+
+let i = 0;
+
+while (true) {
+  currentDrawNumber = drawNumbers[i];
+  console.log(boards);
+
+  markNumbers(currentDrawNumber);
+  checkForBingo();
+  if (boards.length > 1) {
+    removeBingoBoards();
+  } else if (bingo) {
+    break;
   }
-});
+
+  i++;
+}
+
+const calculateBoardScore = (board) => {
+  //get sum of remaining numbers and multiply wit last drawn number
+  console.log('board in calculate score', board);
+  let sumOfUnmarked = 0;
+  board.forEach((obj) => {
+    console.log('sumOfUnmarked', sumOfUnmarked);
+    if (!Object.values(obj)[0]) {
+      console.log('IN IF STATEMENT');
+      sumOfUnmarked += parseInt(Object.keys(obj)[0]);
+    }
+  });
+
+  console.log('currentDrawNumber', currentDrawNumber);
+  const score = sumOfUnmarked * currentDrawNumber;
+  console.log('SCORE', score);
+};
+
+calculateBoardScore(boards[0]);
 
 //INPUT Draw numbers: Array
 //INPUT Boards: string of numbers with rows and empty rows between boards
